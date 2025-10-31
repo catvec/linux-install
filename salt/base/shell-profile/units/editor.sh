94be5701b6924dbc640ab97e078e62bc7df2054d
Noah Huppert <contact@noahh.io>
2025-10-30 21:06:08 -0400

    shell-profile: trying to fix emacs + godot

--- salt/base/shell-profile/units/editor.sh
+++ salt/base/shell-profile/units/editor.sh
@@ -2,4 +2,5 @@
 
 # Open emacs client in existing Emacs window, since this should be running in Emacs
 # in ansi-term by the inferior shell bash.
-export EDITOR="emacsclient --alternate-editor=''"
+export EDITOR="emacsclient -c"
+#--alternate-editor=''"

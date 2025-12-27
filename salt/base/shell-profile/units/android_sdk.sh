b9efaf978accd737bde6491d87ce5d28e7eb59f6
Noah Huppert <contact@noahh.io>
2025-12-27 15:00:47 -0500

    shell-profile: added all paths to android_sdk

--- salt/base/shell-profile/units/android_sdk.sh
+++ salt/base/shell-profile/units/android_sdk.sh
@@ -1,1 +1,10 @@
 export ANDROID_HOME="{{ pillar.android_sdk.sdk_root }}"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
+export PATH="$PATH:$ANDROID_HOME/emulator"
+export PATH="$PATH:$ANDROID_HOME/tools"
+
+# Add all Android SDK build-tools to PATH
+for dir in $ANDROID_HOME/build-tools/*/; do
+    export PATH="$PATH:$dir"
+done

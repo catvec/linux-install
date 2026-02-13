9d5d9fbe7926c625d56f0cc6e90ca1c02ca2d43e
Noah Huppert <contact@noahh.io>
2026-02-13 17:04:10 -0500

    shell-profile: units: prompt: added username

--- salt/base/shell-profile/units/prompt.sh
+++ salt/base/shell-profile/units/prompt.sh
@@ -139,8 +139,8 @@ build_prompt() {
 	   return
     fi
     
-    # EXIT_STATUS HOSTNAME PATH (VENV) %# in cyberpunk purple/blue/cyan
-    export PS1="$(color_fg_magenta)[$(color_reset)$(exit_status_prompt $last_cmd_exit_status)$(color_fg_cyan)\h $(color_fg_blue)\W$(color_fg_magenta)]$(venv_prompt)$(color_reset) $(color_fg_magenta)$(user_symbol)$(color_reset) "
+    # EXIT_STATUS USERNAME HOSTNAME PATH (VENV) %# in cyberpunk purple/blue/cyan
+    export PS1="$(color_fg_magenta)[$(color_reset)$(exit_status_prompt $last_cmd_exit_status)$(color_fg_cyan)\u@\h $(color_fg_blue)\W$(color_fg_magenta)]$(venv_prompt)$(color_reset) $(color_fg_magenta)$(user_symbol)$(color_reset) "
 }
 
 source {{ pillar.bash.preexec.file }}
